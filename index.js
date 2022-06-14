64e5f90ab6127b46222873fef5b9ffe4cd4903d0
Kevin Zheng <kevzheng27@gmail.com>
2022-06-13 20:15:39 -0400

    commit tree 677ec0ec11de72d91ca5e2b867871a68e1fbe38e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newobj = {...obj};
+    newobj[key] = value;
+    return newobj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newobj = {...obj};
+    delete newobj[key];
+    return newobj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
